763ebb661458b9bb00d0c4a00db392c34b681f51
kana-lab <kirinokanatrust@gmail.com>
2022-11-15 15:37:51 +0900

    made label available in immediate instructions

--- src/encoder.rs
+++ src/encoder.rs
@@ -117,7 +117,7 @@ pub fn encode(
                     let dest_addr = address_map.get(s);
                     if dest_addr.is_none() {
                         println!("at line {line}, character {ch}: Syntax Error");
-                        println!("label \"{}\" not found.", label.clone());
+                        println!("label \"{}\" not found.", s.clone());
                         return Err(EncodeError::LabelNotFoundError);
                     }
                     let dest_addr = *dest_addr.unwrap();
