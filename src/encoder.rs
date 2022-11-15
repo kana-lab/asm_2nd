da0af1bb6bc39bbf02e1c0c724f87666ad7586b0
kana-lab <kirinokanatrust@gmail.com>
2022-11-15 15:37:32 +0900

    made label available in immediate instructions

--- src/encoder.rs
+++ src/encoder.rs
@@ -114,7 +114,7 @@ pub fn encode(
                     let n = n as u8;
                     b |= n as u32;
                 } else if let Operand::OpLabel(s) = &operands[2] {
-                    let dest_addr = address_map.get(label);
+                    let dest_addr = address_map.get(s);
                     if dest_addr.is_none() {
                         println!("at line {line}, character {ch}: Syntax Error");
                         println!("label \"{}\" not found.", label.clone());
